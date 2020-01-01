3048c33506f9e15d99aa83f72b97a6e7edee7464
alterui <656812771@qq.com>
2020-01-01 18:25:29 +0800

    Eureka Server set up.

---
+++ eureka-server/src/main/java/com/lr/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.lr.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server 启动类
+ * @author liurui
+ * @date 2020/1/1 18:13
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
