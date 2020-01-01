041ddbfb01bed73942a0b0b5b3a78aa78ab477a8
alterui <656812771@qq.com>
2020-01-01 20:23:24 +0800

    Eureka Server set up.

--- eureka-server/src/main/java/com/lr/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/lr/eureka/server/EurekaServerApplication.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @author liurui
  * @date 2020/1/1 18:13
  */
-@SpringBootApplication
-@EnableEurekaServer
+@SpringBootApplication //该注解的作用是声明该类是Spring Boot服务的入口
+@EnableEurekaServer  //该注解的作用是声明该类是一个Eureka Server 微服务，提供注册和服务发现的作用，即注册中心。类似于dubbo中的zookeeper
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
